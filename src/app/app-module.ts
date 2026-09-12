96d758494b4dea7d8e3a84866e62ba42ea51692a
Migue Contreras <84010@alumnos.utleon.edu.mx>
2026-09-11 18:52:55 -0600

    Form module

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import {FormsModule} from '@angular/forms'
 import { HeoresList } from './heroes/heores-list/heores-list';
 
 @NgModule({
--- src/app/heroes/heores-list/heores-list.ts
+++ src/app/heroes/heores-list/heores-list.ts
@@ -6,4 +6,51 @@ import { Component } from '@angular/core';
   templateUrl: './heores-list.html',
   styleUrl: './heores-list.css',
 })
-export class HeoresList {}
+export class HeoresList {
+
+  imageWidth:number=40;
+  imageMargin:number=2;
+  muestraImagen:boolean=true;
+  listFilter:string='';
+  showImage():void{
+    this.muestraImagen=!this.muestraImagen;
+  };
+
+  heroes:any[]=[
+
+    {
+      imagen:"https://dragonball-api.com/characters/goku_normal.webp",
+      nombre: 'Goku',
+      descripcion: 'kame hame Ha',
+      race: 'Saiyan',
+      ki: 9000
+    },
+
+    {
+      imagen:"https://dragonball-api.com/characters/gohan.webp",
+      nombre: 'Gohan',
+      descripcion: 'Masenko',
+      race: 'Saiyan',
+      ki: 8000
+    },
+
+    {
+      imagen:"https://dragonball-api.com/characters/Krilin_Universo7.webp",
+      nombre: 'Krilin',
+      descripcion: 'Kienzan',
+      race: 'Human',
+      ki: 3000
+    },
+
+    {
+      imagen:"https://dragonball-api.com/characters/Androide_16.webp",
+      nombre: 'Android 17',
+      descripcion: 'Barrier',
+      race: 'Android',
+      ki: 5000
+    }
+
+
+  ]
+
+}
